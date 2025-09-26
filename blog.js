d72457d3021ad17953b9220c797f1969814a2348
jack-rowlands <jackrowlands22@gmail.com>
2025-09-26 23:58:21 +0100

    Add index.html

---
+++ blog.js
@@ -0,0 +1,18 @@
+javascript
+// This is our "database" of blog posts.
+// Add new posts to the TOP of the array.
+
+const blogPosts = [
+  {
+    title: "My Second Post: The Journey Continues",
+    date: "2025-09-27",
+    description: "This is a summary of my second blog post. I'm building on the concepts from the first one and exploring new ideas.",
+    fileName: "my-second-post.html" // The name of the HTML file in the /blog/ folder
+  },
+  {
+    title: "My First Post: Getting Started",
+    date: "2025-09-26",
+    description: "This is a short summary of my very first post on this new blog. It's all about setting things up and the road ahead.",
+    fileName: "my-first-post.html"
+  }
+];
